5f1cf19a9a7e658d412fe92c04a4f7da7de2d596
rharwood <rharwood17@gmail.com>
2023-09-17 17:16:25 -0400

    Uploading changes to outut_writer and some sample test files for PA1

--- Output_Writer.py
+++ Output_Writer.py
@@ -3,6 +3,7 @@ Author: Riley Harwood
 Last Edit: 12 September 2023
 This module will be used as a helpful tool for making input test files in 605.621
 """
+import random
 from typing import TextIO
 
 def process_files(outputFile: TextIO) -> None:
@@ -12,6 +13,9 @@ def process_files(outputFile: TextIO) -> None:
     :param outputFile:
     :return:
     """
-    outputinfo = ""
-    outputFile.write(outputinfo)
-    
+
+    for i in range(1000):
+        x = random.randrange(-100, 100)
+        y = random.randrange(-100, 100)
+        outputFile.write(f"{x},{y}\n")
+
